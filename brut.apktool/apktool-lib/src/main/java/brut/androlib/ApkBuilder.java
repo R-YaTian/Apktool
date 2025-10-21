cc29f9d644d40440bdf62eb637a833b3c49490d8
TY <47445484+R-YaTian@users.noreply.github.com>
2025-10-21 13:37:19 +0800

    zip stream level9

--- brut.apktool/apktool-lib/src/main/java/brut/androlib/ApkBuilder.java
+++ brut.apktool/apktool-lib/src/main/java/brut/androlib/ApkBuilder.java
@@ -419,6 +419,7 @@ public class ApkBuilder {
         LOGGER.info("Building apk file...");
         try (ZipOutputStream out = new ZipOutputStream(Files.newOutputStream(outApk.toPath()))) {
             // Zip aapt output files.
+            out.setLevel(9);
             ZipUtils.zipDir(outDir, out, doNotCompress);
 
             // Zip standard raw files.
